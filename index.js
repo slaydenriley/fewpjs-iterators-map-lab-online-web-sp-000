662938b11c49a6194ee70f598f05dc4a3e05238c
Riley Slayden <slaydenriley@gmail.com>
2020-08-14 14:37:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,4 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = tutorials.map( word => word.charAt(0).toUpperCase() + word.slice(1)) 
